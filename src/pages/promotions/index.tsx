ee11c49f127d902122f0cdc21cc471ed88a2021c
yancarlodev <yancarlodc@gmail.com>
2022-11-09 23:45:48 -0300

    feat: add promotions page and interactive

---
+++ src/pages/promotions/index.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import BackgroundDashboard from "../../components/BackgroundDashboard";
+import GeralContainer from "../../components/GeralContainer";
+import HeaderDashboard from "../../components/HeaderDashboard";
+import LeftAside from "../../components/LeftAside";
+import RightSide from "../../components/RightSide";
+import SEO from "../../components/SEO";
+import PromotionCard from '../../components/PromotionCard'
+import { getCookie } from 'cookies-next'
+import axios from 'axios'
+
+const Promotions = ({ promotions }) => {
+  return (
+    <BackgroundDashboard config="flex flex-col">
+      <SEO
+        title="Dashboard"
+        description="The NEXUS App simplifies your access to your games, unifying all platforms into one."
+      />
+      <GeralContainer>
+        <LeftAside />
+
+        <RightSide>
+          <HeaderDashboard title="Promotions" />
+
+          <main>
+            <ul className="grid grid-cols-3 gap-4 h-[600px] overflow-y-auto">
+                {
+                    promotions.map(promotion => (
+                        <PromotionCard promotion={promotion} key={promotion.id}/>
+                    ))
+                }
+            </ul>
+          </main>
+        </RightSide>
+      </GeralContainer>
+    </BackgroundDashboard>
+  );
+};
+
+export default Promotions;
+
+export const getStaticProps = async ({req, res}) => {
+    const token = getCookie("token", { req, res });
+
+    const response = await axios.get('https://nexus-gamestand-api.herokuapp.com/promotions', {
+        headers: {
+            authorization: `Bearer ${token}`
+        }
+    })
+    
+    return {
+        props: {
+            promotions: response.data
+        },
+        revalidate: 10
+    }
+}
