d63e6e383e91dd8ff99e6100a0c19049e0aaed6b
ssidartaa <odssy351@gmail.com>
2022-11-10 10:15:25 -0500

    feat: bug report page

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
     colors: {
       defaulttextdark: "#FFFFFF",
       boxcolordark: "#131A39",
-      placeholder: "##E1E1E1",
+      placeholder: "#E1E1E1",
       primarycolor: "#E6911A",
       secondcolor: "#E39830",
       thirdcolor: "#E1B06B",
@@ -48,7 +48,7 @@ module.exports = {
     },
     fontFamily: {
       inter: ["Inter", "ui-monospace", "SFMono-Regular"],
-      bebas: ["Bebas Neue", "ui-monospace", "SFMono-Regular"]
+      bebas: ["Bebas Neue", "ui-monospace", "SFMono-Regular"],
     },
     fontSize: {
       title1: "2.5rem",
@@ -66,10 +66,13 @@ module.exports = {
     },
     extend: {
       boxShadow: {
-        'gayPride': 'rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px;',
-        'gayPride2': 'rgb(255, 85, 85) 0px 0px 0px 3px, rgb(255, 156, 85) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(31, 193, 27) 0px 0px 0px 12px, rgb(85, 91, 255) 0px 0px 0px 15px;',
-        'loginShadow': 'rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;'
-      }
+        gayPride:
+          "rgb(85, 91, 255) 0px 0px 0px 3px, rgb(31, 193, 27) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(255, 156, 85) 0px 0px 0px 12px, rgb(255, 85, 85) 0px 0px 0px 15px;",
+        gayPride2:
+          "rgb(255, 85, 85) 0px 0px 0px 3px, rgb(255, 156, 85) 0px 0px 0px 6px, rgb(255, 217, 19) 0px 0px 0px 9px, rgb(31, 193, 27) 0px 0px 0px 12px, rgb(85, 91, 255) 0px 0px 0px 15px;",
+        loginShadow:
+          "rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;",
+      },
     },
   },
   plugins: [],
